0f4f711a14754908afd61f30846bda0b7eda45b9
Joshua Mumford <JMumford45@gmail.com>
2020-10-29 22:26:10 -0700

    localVersion

---
+++ MetalSlug(Prototype)/Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//used to manage frequently used functions such as spawning and destroying objects
+public class GameManager : MonoBehaviour
+{
+    //create partial singleton to prevent multiple instances
+    public static GameManager gm;
+
+    private void Start()
+    {
+        if (gm == null)
+            gm = FindObjectOfType<GameManager>();
+    }
+
+    public GameObject playerPrefab;
+    public Transform spawnPoint;
+    public float spawnDelay = 1.5f;
+    public GameObject currentCheckpoint;
+    public IEnumerator RespawnPlayer(GameObject player)
+    {
+        Debug.Log("Add spawn sound and animation");
+        yield return new WaitForSeconds(spawnDelay);
+
+        Debug.Log(playerPrefab.name);
+        //Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        player.transform.position = currentCheckpoint.transform.position;
+        player.SetActive(true);
+        Debug.Log("add effects");
+    }
+
+    //methods to kill player and enemy 
+    public static void KillEnemy(EnemyBehaviour enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+    
+    public static void KillPlayer(GameObject player)
+    {
+        player.SetActive(false);
+        gm.RespawnPlayer(player);
+    }
+}
---
+++ MetalSlug(Prototype)/Assets/Scripts/EnemyBehaviour/PlatformPatrol.cs
@@ -0,0 +1,70 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlatformPatrol : MonoBehaviour
+{
+    EnemyBehaviour.EnemyStats _stats = new EnemyBehaviour.EnemyStats();
+    private Animator animate;
+    private Rigidbody2D rb;
+    public float speed;
+    private float distance = 1.0f;
+    private bool movingRight = true;
+    private RaycastHit2D hit;
+    public Transform groundDetection;
+    bool isMoving;
+
+    public void Start()
+    {
+        speed = _stats.speed;
+        animate = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+       // Debug.Log(animate.name);
+        isMoving = EnemyBehaviour.isMoving;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        patrol();
+    }
+    void patrol()
+    {
+        isMoving = animate.GetBool("isMoving");
+        if (isMoving == true)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            //animate.Play("walk");
+        }
+        else
+            stopPatrol(isMoving);
+
+        hit = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+        if (hit.collider == null)
+        {
+           // Debug.Log(hit.collider);
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+            //Debug.Log(hit.collider.name);
+    }
+
+    void stopPatrol(bool not_moving)
+    {
+        if(not_moving)
+        {
+            rb.velocity = Vector2.zero;
+            animate.SetBool("isMoving", false);
+        }
+    }
+}
+
