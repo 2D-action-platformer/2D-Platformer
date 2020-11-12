eb9c191708585941a1e0338645cc039d860f7079
jgarcia687 <jgarcia68777@gmail.com>
2020-11-12 18:07:39 -0500

    HP bar + Player Damage

---
+++ MetalSlug(Prototype)/Assets/Scripts/Gameplay/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public PlayerController playerController;
+    public float MaxHealth = 10f;
+    public float CurrentHealth = 10f;
+    private BoxCollider2D bc2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bc2d = GetComponent<BoxCollider2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CurrentHealth == 0)
+        {
+            playerController.enabled = false;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            CurrentHealth = CurrentHealth - 1;
+        }
+    }
+}
